8707870d95450f7aaf1f6ea5a6b6196970b9b6a3
Gabriel Falcão <gabriel@nacaolivre.org>
2020-03-08 02:31:47 +0100

    New release: 0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ copyright = "2020, Gabriel Falcão"
 author = "Gabriel Falcão"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.3"
+release = "0.0.4"
 
 
 # -- General configuration ---------------------------------------------------
