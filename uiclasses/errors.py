70e031e2019ea889327548fdf484daf75322c907
Gabriel Falcão <gabriel@nacaolivre.org>
2020-03-08 04:09:11 +0100

    run black

--- uiclasses/errors.py
+++ uiclasses/errors.py
@@ -18,6 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
 class UIClassesException(Exception):
     """base exception for anything raised within this python package
     """
