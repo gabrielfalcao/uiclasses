b6b9122f9e3d08402b201882be14b110152a39d1
Gabriel Falcão <gabriel@nacaolivre.org>
2020-03-21 03:07:08 +0100

    New release: 2.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ copyright = "2020, NewStore GmbH"
 author = "NewStore GmbH"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.1"
+release = "2.0.0"
 
 
 # -- General configuration ---------------------------------------------------
